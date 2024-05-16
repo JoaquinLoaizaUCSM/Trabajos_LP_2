30d3eb63e81bed9f1cd50dcb0dcf39818dce7161
Yhosfer <yhosfer.quispe@ucsm.edu.pe>
2024-05-15 21:47:49 -0500

    Ejercicio_1.py

---
+++ Trabajo_7/PY/Ejercicios/Ejercicio_2.py
@@ -0,0 +1,44 @@
+class Persona:
+    def __init__(self, nombre, salario, correo_electronico):
+        self.nombre = nombre
+        self.salario = salario
+        self.correo_electronico = correo_electronico
+
+    def set_correo_electronico(self, correo):
+        self.correo_electronico = correo
+
+    def get_correo_electronico(self):
+        return self.correo_electronico
+
+class Empleado(Persona):
+    def __init__(self, nombre, salario, correo_electronico, departamento):
+        Persona.__init__(self, nombre, salario, correo_electronico)
+        self.departamento = departamento
+
+    def __str__(self):
+        return (f"Nombre: {self.nombre}\n"
+                f"Salario: {self.salario}\n"
+                f"Correo electronico: {self.correo_electronico}\n"
+                f"Departamento: {self.departamento}\n")
+
+class Cliente(Persona):
+    def __init__(self, nombre, salario, correo_electronico, cuentaBancaria):
+        Persona.__init__(self, nombre, salario, correo_electronico)
+        self.cuentaBancaria = cuentaBancaria
+
+    def __str__(self):
+        return (f"Nombre: {self.nombre}\n"
+                f"Salario: {self.salario}\n"
+                f"Correo electronico: {self.correo_electronico}\n"
+                f"Cuenta Bancaria: {self.cuentaBancaria}\n")
+
+empleado = Empleado("Yhosfer", 5000, "yhosfer@gmail.com","123")
+print("Acceso a correo electronico del empleado")
+print(empleado.get_correo_electronico()+"\n")
+
+print(empleado)
+
+cliente = Cliente("Anderson", 10000, "anderson@gmail.com", "54322")
+print("Acceso a correo electronico del Cliente")
+print(cliente.get_correo_electronico()+"\n")
+print(cliente)

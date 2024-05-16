bda83610b797be2ab33d03b5fcb6bc21fff93db6
Yhosfer <yhosfer.quispe@ucsm.edu.pe>
2024-05-15 19:11:49 -0500

    act 2, defini atributos de la clase base como protegidos y metodo protegigo de encriptación

--- Trabajo_6/CPP/Actividades/Actividad_3.cpp
+++ Trabajo_6/CPP/Actividades/Actividad_3.cpp
@@ -1,45 +1,131 @@
 #include <iostream>
-#include <string>
-
 
 using namespace std;
 
-class Banco {
-private:
-    static double tasaInteres;
+class cliente{
+
+protected:
+    string nombre;
+    int telefono{};
+    string correo;
+    string cuentaBancaria;
+    double monto{};
+
+    static string encriptarInformacion(string informacion, int desplazamiento=3) {
+        string resultado;
+        for (int i = 0; i < informacion.length(); i++) {
+            if (isdigit(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 48) % 10 + 48);
+            else if (isupper(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 65) % 26 + 65);
+            else
+                resultado += char(int(informacion[i] + desplazamiento - 97) % 26 + 97);
+        }
+        return resultado;
+    }
 
 public:
 
-    static void cambiarTasaInteres(double tasa) {
-        tasaInteres = tasa;
+    cliente(string nombre, int telefono, const string& correo, const string& cuentaBancaria, double monto){
+        this -> nombre = nombre;
+        this -> telefono = telefono;
+        this -> correo = correo;
+        this -> cuentaBancaria = cuentaBancaria;
+        this -> monto = monto;
     }
-    static double obtenerTasaInteres(){
-        return tasaInteres;
-    }
-};
-double Banco::tasaInteres=0.0;
 
-class CuentaBancaria{
-private:
-    double saldo;
-public:
-    CuentaBancaria(double saldoInicial) : saldo(saldoInicial) {}
-    void depositar(double cantidad){
-        saldo= saldo + cantidad;
+
+    friend ostream &operator<<(ostream &os, const cliente &cliente) {
+        os << "Nombre: " << cliente.nombre << "\n"
+           << "Telefono: " << cliente.telefono << "\n"
+           << "Correo: " << cliente.correo << "\n"
+           << "Cuenta Bancaria: " << cliente.cuentaBancaria << "\n"
+           << "Monto: " << cliente.monto;
+        return os;
     }
-    void retirar(double cantidad){
-    if (saldo > cantidad){
-        saldo= saldo - cantidad;
-    } else{
-        cout<<"no tienes plata maldito mendigo"<<endl;
+    ~cliente(){
+        cout << "Destructor Cliente ejecutado." << endl;
     }
-    }
+
 };
+
+
+
+class clienteSeguro : public cliente {
+private:
+    string claveEncriptada;
+
+    static string descriptarInformacion(string informacion, int desplazamiento=3) {
+        string resultado;
+        for (int i = 0; i < informacion.length(); i++) {
+            if (isdigit(informacion[i]))        //verificar si es un digito
+                resultado += char(int(informacion[i] - desplazamiento - 48 + 10) % 10 + 48);
+            else if (isupper(informacion[i]))   // verificar si es una letra mayuscula
+                resultado += char(int(informacion[i] - desplazamiento - 65 + 26) % 26 + 65);
+            else                                   // verificar si es una letra minuscula
+                resultado += char(int(informacion[i] - desplazamiento - 97 + 26) % 26 + 97);
+        }
+        return resultado;
+    }
+
+
+public:
+    clienteSeguro(string nombre, int telefono, string correo, string cuentaBancaria, double monto, string clave)
+            : cliente(encriptarInformacion(nombre), telefono, encriptarInformacion(correo), encriptarInformacion(cuentaBancaria), monto){
+
+        this -> claveEncriptada = encriptarInformacion(std::move(clave));
+
+    }
+
+    string getClaveEncriptada()
+    {
+        return claveEncriptada;
+    }
+
+    static string encriptarInformacion(string informacion, int desplazamiento=3) {
+        string resultado;
+        for (int i = 0; i < informacion.length(); i++) {
+            if (isdigit(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 48) % 10 + 48);
+            else if (isupper(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 65) % 26 + 65);
+            else
+                resultado += char(int(informacion[i] + desplazamiento - 97) % 26 + 97);
+        }
+        return resultado;
+    }
+
+    bool verificarAutenticidad(const string& claveIngresada) {
+        return claveIngresada == descriptarInformacion(claveEncriptada);
+    }
+    ~clienteSeguro() {
+        cout << "Destructor Cliente seguro ejecutado." << endl;
+    };
+};
+
+
 
 int main(){
-Banco::cambiarTasaInteres(0.10);
-CuentaBancaria cuenta(200);
-cuenta.depositar(199);
-cuenta.retirar(500);
 
+    clienteSeguro grossman = clienteSeguro("Juan Perez",
+                                           987654321,
+                                           "Juan.Perez@gmail.com",
+                                           "123456789",
+                                           1000.0,
+                                           "1234");
+
+
+    cout << grossman << endl;
+
+    cout << "Clave encriptada: " << grossman.getClaveEncriptada() << endl;
+
+    cout << "Ingrese la clave para verificar la autenticidad:" << endl;
+    string claveIngresada;
+    cin >> claveIngresada;
+
+    if (grossman.verificarAutenticidad(claveIngresada)) {
+        cout << "La clave ingresada es correcta" << endl;
+    } else {
+        cout << "La clave ingresada es incorrecta" << endl;
+    }
 }

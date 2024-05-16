517fd6b3b1b7d24139f369350b3861b9fdceada7
Yhosfer <yhosfer.quispe@ucsm.edu.pe>
2024-05-15 19:17:50 -0500

    Es una copia de la acti 3

--- Trabajo_6/CPP/Actividades/Actividad_4.cpp
+++ Trabajo_6/CPP/Actividades/Actividad_4.cpp
@@ -2,44 +2,130 @@
 
 using namespace std;
 
-class Vector{
-public:
-    int x;
-    int y;
-    Vector(int x=0, int y=0){
-        this->x=x;
-        this->y=y;
+class cliente{
+
+protected:
+    string nombre;
+    int telefono{};
+    string correo;
+    string cuentaBancaria;
+    double monto{};
+
+    static string encriptarInformacion(string informacion, int desplazamiento=3) {
+        string resultado;
+        for (int i = 0; i < informacion.length(); i++) {
+            if (isdigit(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 48) % 10 + 48);
+            else if (isupper(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 65) % 26 + 65);
+            else
+                resultado += char(int(informacion[i] + desplazamiento - 97) % 26 + 97);
+        }
+        return resultado;
     }
 
-    Vector operator + (Vector otroVector){
-        this->x += otroVector.x;
-        this->y += otroVector.y;
-        return {};
-    };
+public:
 
-    Vector operator - (Vector otroVector){
-        this->x -= otroVector.x;
-        this->y -= otroVector.y;
-        return {};
+    cliente(string nombre, int telefono, const string& correo, const string& cuentaBancaria, double monto){
+        this -> nombre = nombre;
+        this -> telefono = telefono;
+        this -> correo = correo;
+        this -> cuentaBancaria = cuentaBancaria;
+        this -> monto = monto;
+    }
+
+
+    friend ostream &operator<<(ostream &os, const cliente &cliente) {
+        os << "Nombre: " << cliente.nombre << "\n"
+           << "Telefono: " << cliente.telefono << "\n"
+           << "Correo: " << cliente.correo << "\n"
+           << "Cuenta Bancaria: " << cliente.cuentaBancaria << "\n"
+           << "Monto: " << cliente.monto;
+        return os;
+    }
+    ~cliente(){
+        cout << "Destructor Cliente ejecutado." << endl;
+    }
+
+};
+
+
+
+class clienteSeguro : public cliente {
+private:
+    string claveEncriptada;
+
+    static string descriptarInformacion(string informacion, int desplazamiento=3) {
+        string resultado;
+        for (int i = 0; i < informacion.length(); i++) {
+            if (isdigit(informacion[i]))        //verificar si es un digito
+                resultado += char(int(informacion[i] - desplazamiento - 48 + 10) % 10 + 48);
+            else if (isupper(informacion[i]))   // verificar si es una letra mayuscula
+                resultado += char(int(informacion[i] - desplazamiento - 65 + 26) % 26 + 65);
+            else                                   // verificar si es una letra minuscula
+                resultado += char(int(informacion[i] - desplazamiento - 97 + 26) % 26 + 97);
+        }
+        return resultado;
+    }
+
+
+public:
+    clienteSeguro(string nombre, int telefono, string correo, string cuentaBancaria, double monto, string clave)
+            : cliente(encriptarInformacion(nombre), telefono, encriptarInformacion(correo), encriptarInformacion(cuentaBancaria), monto){
+
+        this -> claveEncriptada = encriptarInformacion(std::move(clave));
+
+    }
+
+    string getClaveEncriptada()
+    {
+        return claveEncriptada;
+    }
+
+    static string encriptarInformacion(string informacion, int desplazamiento=3) {
+        string resultado;
+        for (int i = 0; i < informacion.length(); i++) {
+            if (isdigit(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 48) % 10 + 48);
+            else if (isupper(informacion[i]))
+                resultado += char(int(informacion[i] + desplazamiento - 65) % 26 + 65);
+            else
+                resultado += char(int(informacion[i] + desplazamiento - 97) % 26 + 97);
+        }
+        return resultado;
+    }
+
+    bool verificarAutenticidad(const string& claveIngresada) {
+        return claveIngresada == descriptarInformacion(claveEncriptada);
+    }
+    ~clienteSeguro() {
+        cout << "Destructor Cliente seguro ejecutado." << endl;
     };
 };
 
-int main() {
-    Vector coordenadas1 = Vector(2,4);
-    Vector coordenadas2 = Vector(5,6);
 
-    cout << "Operador -" << endl;
-    coordenadas1-coordenadas2;
 
-    cout << "Coordenadas " <<"x: "<< coordenadas1.x <<endl
-         << "Coordenadas " <<"y: "<< coordenadas1.y << endl;
+int main(){
 
-    cout << "Operador +" << endl;
-    coordenadas1+coordenadas2;
+    clienteSeguro grossman = clienteSeguro("Juan Perez",
+                                           987654321,
+                                           "Juan.Perez@gmail.com",
+                                           "123456789",
+                                           1000.0,
+                                           "1234");
 
-    cout << "Coordenadas " <<"x: "<< coordenadas1.x <<endl
-         << "Coordenadas " <<"y: "<< coordenadas1.y << endl;
 
-    return 0;
+    cout << grossman << endl;
+
+    cout << "Clave encriptada: " << grossman.getClaveEncriptada() << endl;
+
+    cout << "Ingrese la clave para verificar la autenticidad:" << endl;
+    string claveIngresada;
+    cin >> claveIngresada;
+
+    if (grossman.verificarAutenticidad(claveIngresada)) {
+        cout << "La clave ingresada es correcta" << endl;
+    } else {
+        cout << "La clave ingresada es incorrecta" << endl;
+    }
 }
-// comentario commit y pushh

6aff4b20071ba90dac3cc26b7ed8dd40b76cfe7d
Yhosfer <yhosfer.quispe@ucsm.edu.pe>
2024-05-22 19:05:21 -0500

    Ejercicio_2.py Finalizado

--- Trabajo_8/PY/Ejercicios/Ejercicio_2.py
+++ Trabajo_8/PY/Ejercicios/Ejercicio_2.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import time
+
 
 class Reproductor(ABC):
     @abstractmethod
@@ -11,7 +13,11 @@ class Reproductor(ABC):
     def detener(self):
         pass
     @abstractmethod
-    def iniciar(self):
+    def encender(self):
+        pass
+
+    @abstractmethod
+    def apagar(self):
         pass
 
 class ReproductorMP3(Reproductor):
@@ -24,7 +30,7 @@ class ReproductorMP3(Reproductor):
     def reproducir(self):
         if self.modo == True:
             if self.estado == True:
-                print("El archivo mp3 ya se encuentra en reproduccion")
+                print("Reanunando reproduccion")
             else:
                 self.modo = True
                 print(f"Reproduciendo archivo mp3")
@@ -33,25 +39,74 @@ class ReproductorMP3(Reproductor):
 
     def pausar(self):
         if self.modo == True:
-            if self.estado == True:
-                self.estado = False
-                print("Pausando archivo mp3")
+            if self.estado == False:
+                self.estado = True
+                print("Pausando reproduccion")
             else:
-                print("Archivo m'3 ya en pausa")
+                print("Archivo mp3 ya en pausa")
         else:
             print("Reproductor apagado")
 
     def detener(self):
         if self.modo == True:
             self.estado = False
-            print("Dteneiendo Reproduccion de musica")
+            print("Deteniendo Reproduccion de musica")
         else:
             print("Dispositivo apagado")
 
-    def iniciar(self):
+    def encender(self):
+        if self.modo == False:
+            self.modo = True
+            print("Encendiendo MP3...")
+
+    def apagar(self):
         if self.modo == True:
-            print("Encendiendo MP3")
-def menu_():
-    print("Menu acciones Reproductor")
+            self.modo = False
+            self.estado = False
+            print("Apagando Reproductor")
+
+def menu():
+    print(" ***** Menu ***** ")
     print("1. Encender Reproductor")
-    print("2. Reproducir Reproductor")
+    print("2. Reproducir archivo Mp3")
+    print("3. Pausar archivo Mp3")
+    print("4. Detener archivo Mp3 ")
+    print("5. Apagar Reproductor")
+
+def menu_acciones():
+    print("Menu Acciones")
+    print("1. Reproducir archivo Mp3")
+    print("2. Pausar archivo Mp3")
+    print("3. Detener archivo Mp3 ")
+
+if __name__ == "__main__":
+    mp3 = ReproductorMP3()
+    while True:
+        menu()
+        op = int(input("Ingrese una opcion: "))
+        if op == 1:
+            mp3.encender()
+
+
+        elif op == 2:
+            mp3.reproducir()
+
+
+        elif op == 3:
+            mp3.pausar()
+
+
+        elif op == 4:
+            mp3.detener()
+
+
+        elif op == 5:
+            mp3.apagar()
+            break
+        else:
+            print("Opcion invalida")
+        print("\n")
+        time.sleep(1.5)
+
+
+

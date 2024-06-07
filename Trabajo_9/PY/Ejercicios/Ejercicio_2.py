906f965a0096c2d9328887322c9ba52ca601eab3
Yhosfer <yhosfer.quispe@ucsm.edu.ppe>
2024-06-07 17:50:33 -0500

    Completado

---
+++ Trabajo_9/PY/Ejercicios/Ejercicio_2.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def comer(self):
+        pass
+    @abstractmethod
+    def dormir(self):
+        pass
+    @abstractmethod
+    def moverse(self):
+        pass
+
+class Mamifero(Animal):
+    def comer(self):
+        print("El mamifero esta comiendo")
+
+    def dormir(self):
+        print("El mamifero esta durmiendo")
+
+    def moverse(self):
+        print("El mamifero esta caminando")
+
+class Ave(Animal):
+    def comer(self):
+        print("El ave esta comiendo")
+
+    def dormir(self):
+        print("El ave esta durmiendo")
+
+
+    def moverse(self):
+        print("El ave est volando")
+
+class Reptil(Animal):
+    def comer(self):
+        print("El reptil esta comiendo")
+
+    def dormir(self):
+        print("El reptil est@ durmiendo")
+
+    def moverse(self):
+        print("El reptil esta reptando.")
+
+class Anfibio(Animal):
+    def comer(self):
+        print("El anfibio esta comiendo")
+
+    def dormir(self):
+        print("El anfibio esta durmiendo")
+
+    def moverse(self):
+        print("El anfibio esta saltando...")
+
+
+if __name__ == "__main__":
+    animales = []
+    animales.append(Anfibio())
+    animales.append(Reptil())
+    animales.append(Ave())
+    animales.append(Mamifero())
+
+    for animal in animales:
+        animal.comer()
+        animal.dormir()
+        animal.moverse()

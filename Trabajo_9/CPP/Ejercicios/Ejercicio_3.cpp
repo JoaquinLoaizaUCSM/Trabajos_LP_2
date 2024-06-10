a8435d5e08214fb846df67854cd0a10cc8ed1f7a
Grossman Vargas <grossman.vargas@ucsm.edu.pe>
2024-06-10 08:48:39 -0500

    Se crean los archivos de los ejercicios y se realiza parte del numero 1

--- Trabajo_9/CPP/Ejercicios/Ejercicio_3.cpp
+++ Trabajo_9/CPP/Ejercicios/Ejercicio_3.cpp
@@ -1,3 +1,267 @@
-//
-// Created by Grossman on 05/06/2024.
-//
+#include <iostream>
+using namespace std;
+
+class Empleados{
+public:
+    virtual void calcularSalario() const = 0;
+    virtual void agregarBono() = 0;
+};
+
+void menu(){
+    ;
+}
+class Asalariados : public Empleados{
+private:
+    string nombre;
+    string apellido;
+    int codigo;
+    int bono;
+    int const salario = 1000;
+
+public:
+    Asalariados(string n , string l, int code, int b = 0){
+        this -> nombre = n;
+        this -> apellido = l;
+        this -> codigo = code;
+        this -> bono = b;
+    }
+
+    void calcularSalario() const override{
+        cout << "Salario -> " << salario + bono << endl;
+    }
+
+    void agregarBono() override{
+        int extra;
+        cout << "" << endl;
+
+    }
+
+    void calcularArea() const override{
+        cout << "Area: " << 3.14 * (radio * radio) << endl;
+    }
+
+    void calcularPerimetro() const override{
+        cout << "Perimetro: " << 3.14 * (diametro) << endl;
+    }
+
+    void especificaciones() const override{
+        cout << "Datos del Circulo: " << endl;
+        cout << "Radio -> " << this -> radio << endl;
+        cout << "Diametro -> " << this -> diametro << endl;
+        this -> calcularPerimetro();
+        this -> calcularArea();
+    }
+};
+
+
+class Rectangulo: public Forma{
+private:
+    int base;
+    int altura;
+
+public:
+    Rectangulo(int b = 0, int h = 0){
+        this -> base = b;
+        this -> altura = h;
+    }
+
+    void ingresarPropiedades() override{
+        int b;
+        int h;
+        cout << "Ingrese la nueva base -> ";
+        cin >> b;
+        cout << "Ingrese la nueva altura -> ";
+        cin >> h;
+        this -> base = b;
+        this -> altura = h;
+    }
+
+    void calcularArea() const override{
+        cout << "Area: " << base * altura << endl;
+    }
+
+    void calcularPerimetro() const override{
+        cout << "Perimetro: " << 2*(base+altura) << endl;
+    }
+
+    void especificaciones() const override{
+        cout << "Datos del Rectangulo: " << endl;
+        cout << "Base -> " << this -> base << endl;
+        cout << "Altura -> " << this -> altura << endl;
+        this -> calcularPerimetro();
+        this -> calcularArea();
+    }
+};
+
+
+class Triangulo: public Forma{
+private:
+    int base;
+    int altura;
+    int hipotenusa;
+
+public:
+    Triangulo(int b = 0, int h = 0){
+        this -> base = b;
+        this -> altura = h;
+        this -> hipotenusa = sqrt((b*b)+(h*h));
+    }
+
+    void ingresarPropiedades() override{
+        int b;
+        int h;
+        cout << "Ingrese la nueva base -> ";
+        cin >> b;
+        cout << "Ingrese la nueva altura -> ";
+        cin >> h;
+        this -> base = b;
+        this -> altura = h;
+        this -> hipotenusa = sqrt((b*b)+(h*h));
+    }
+
+    void calcularArea() const override{
+        cout << "Area: " << (base * altura)/2 << endl;
+    }
+
+    void calcularPerimetro() const override{
+        cout << "Perimetro: " << (base+altura+hipotenusa) << endl;
+    }
+
+    void especificaciones() const override{
+        cout << "Datos del Triangulo: " << endl;
+        cout << "Base -> " << this -> base << endl;
+        cout << "Altura -> " << this -> altura << endl;
+        cout << "Hipotenusa -> " << this -> hipotenusa << endl;
+        this -> calcularPerimetro();
+        this -> calcularArea();
+    }
+};
+
+class Trapecio: public Forma{
+private:
+    int baseMayor;
+    int baseMenor;
+    int altura;
+    int lado1;
+    int lado2;
+
+public:
+    Trapecio(int bMayor = 0, int bMenor = 0, int h = 0, int l1 = 0, int l2 = 0){
+        this -> baseMayor = bMayor;
+        this -> baseMenor = bMenor;
+        this -> altura = h;
+        this -> lado1 = l1;
+        this -> lado2 = l2;
+    }
+
+    void ingresarPropiedades() override{
+        int bMayor, bMenor, h, l1, l2;
+        cout << "Ingrese la nueva base mayor -> ";
+        cin >> bMayor;
+        cout << "Ingrese la nueva base menor -> ";
+        cin >> bMenor;
+        cout << "Ingrese la nueva altura -> ";
+        cin >> h;
+        cout << "Ingrese el nuevo lado 1 -> ";
+        cin >> l1;
+        cout << "Ingrese el nuevo lado 2 -> ";
+        cin >> l2;
+        this -> baseMayor = bMayor;
+        this -> baseMenor = bMenor;
+        this -> altura = h;
+        this -> lado1 = l1;
+        this -> lado2 = l2;
+    }
+
+    void calcularArea() const override{
+        cout << "Area: " << ((baseMayor + baseMenor) * altura) / 2 << endl;
+    }
+
+    void calcularPerimetro() const override{
+        cout << "Perimetro: " << baseMayor + baseMenor + lado1 + lado2 << endl;
+    }
+
+    void especificaciones() const override{
+        cout << "Datos del Trapecio: " << endl;
+        cout << "Base Mayor -> " << this -> baseMayor << endl;
+        cout << "Base Menor -> " << this -> baseMenor << endl;
+        cout << "Altura -> " << this -> altura << endl;
+        cout << "Lado 1 -> " << this -> lado1 << endl;
+        cout << "Lado 2 -> " << this -> lado2 << endl;
+        this -> calcularPerimetro();
+        this -> calcularArea();
+    }
+};
+
+void menu_figuras(){
+    cout << "Crear: " << endl;
+    cout << "0 -> Finalizar Programa" << endl;
+    cout << "1 -> Circulo" << endl;
+    cout << "2 -> Rectangulo" << endl;
+    cout << "3 -> Triangulo" << endl;
+    cout << "4 -> Trapecio" << endl;
+}
+
+bool validacion(int i, int end){
+    for (int x = 0; x < end; x++){
+        if ( x == i ){
+            return true;
+        }
+    }
+    return false;
+}
+
+//Menu provisional en caso de querer expandir el codigo con un menu interno de figuras
+void menu_interno(){
+    cout << "0 -> Salir" << endl;
+    cout << "1 -> Calcular Perimetro" << endl;
+    cout << "2 -> Calcular Area" << endl;
+    cout << "3 -> Mostrar Detalles" << endl;
+}
+
+int main() {
+    while (true) {
+        menu_figuras();
+        int opc;
+        cout << "Ingrese una opcion: " << endl;
+        cin >> opc;
+        while (!validacion(opc, 5)) {
+            cout << "Ingrese una opcion: " << endl;
+            cin >> opc;
+        }
+        switch (opc) {
+            case 0: {
+                return 0;
+            }
+
+            case 1: {
+                Circulo circulito;
+                circulito.ingresarPropiedades();
+                circulito.especificaciones();
+                break;
+            }
+
+            case 2: {
+                Rectangulo rectangulito;
+                rectangulito.ingresarPropiedades();
+                rectangulito.especificaciones();
+                break;
+            }
+
+            case 3: {
+                Triangulo triangulito;
+                triangulito.ingresarPropiedades();
+                triangulito.especificaciones();
+                break;
+            }
+
+            case 4: {
+                Trapecio trapecito;
+                trapecito.ingresarPropiedades();
+                trapecito.especificaciones();
+                break;
+            }
+
+        }
+    }
+}

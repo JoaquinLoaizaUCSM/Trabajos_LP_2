ec7175433472c68cb34cbeb2379e30ec1480c548
Grossman Vargas <grossman.vargas@ucsm.edu.pe>
2024-06-16 09:14:28 -0500

    Se completaron los ejercicios 1,2,3 y se realizaron sus respectivos main

--- Trabajo_10/CPP/Ejercicios/Ejercicio_1.cpp
+++ Trabajo_10/CPP/Ejercicios/Ejercicio_1.cpp
@@ -1,3 +1,38 @@
-//
-// Created by USER on 14/06/2024.
-//
+#include <iostream>
+using namespace std;
+#include <vector>
+
+template <typename T>
+void ordenamiento_vector(vector<T> &vct) {
+    int tamaño = vct.size();
+    for (int i = 0; i < tamaño - 1; i++) {
+        for (int j = 0; j < tamaño - i - 1; j++) {
+            if (vct[j] > vct[j + 1]) {
+                T temp = vct[j];
+                vct[j] = vct[j + 1];
+                vct[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    vector<int> numeritos = {1, 2, 3, 0, 9, 7, 6};
+    ordenamiento_vector(numeritos);
+
+    for (int i = 0; i < numeritos.size(); i++){
+        cout << numeritos[i];
+    }
+
+    cout << "\n" << endl;
+
+    vector<float> numeritos_flotantes = {6.5, 45.7, 85.5, 2.2, 1.1, 2.5, 4.9};
+    ordenamiento_vector(numeritos_flotantes);
+
+    for (int i = 0; i < numeritos_flotantes.size(); i++){
+        cout << numeritos_flotantes[i];
+    }
+
+
+    return 0;
+};

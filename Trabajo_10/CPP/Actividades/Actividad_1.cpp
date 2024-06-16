a7831a517adde47d94e7be981037ceedd7b73746
Grossman Vargas <grossman.vargas@ucsm.edu.pe>
2024-06-16 08:51:52 -0500

    Se completaron las actividades 1,2,3 y se realizaron sus respectivos main

--- Trabajo_10/CPP/Actividades/Actividad_1.cpp
+++ Trabajo_10/CPP/Actividades/Actividad_1.cpp
@@ -1,3 +1,62 @@
-//
-// Created by USER on 14/06/2024.
-//
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T calculo_distancia(T d1, T d2, T d3, T d4){
+    T dt = ((d2-d1) + (d3-d2) + (d4-d3));
+    cout << "La distancia total a recorrer es de " << dt << " km."<< endl;
+    return dt;
+}
+
+template <typename T>
+T tiempo_entrega(T dt, T vel){
+    cout << "El tiempo de entrega estimado es de " << dt/vel << " horas."<< endl;
+    return dt/vel;
+}
+
+template <typename T>
+T realizar_informe(){
+    T nI, d1, d2, d3, d4, dt, tf, vel;
+
+    cout << "Indique el numero de informe : " << endl;
+    cin >> nI;
+
+    cout << "Ingrese la distancia n1 : " << endl;
+    cin >> d1;
+
+    cout << "Ingrese la distancia n2 : " << endl;
+    cin >> d2;
+
+    cout << "Ingrese la distancia n3 : " << endl;
+    cin >> d3;
+
+    cout << "Ingrese la distancia n4 : " << endl;
+    cin >> d4;
+
+    dt = calculo_distancia(d1 ,d2 ,d3 ,d4);
+
+    cout << "Ingrese la velocidad a la que se ira : " << endl;
+    cin >> vel;
+
+    tf = tiempo_entrega(dt,vel);
+
+    cout << "Informe de ruta: " << endl;
+    cout << "Distancia recorrida        -> " << dt << endl;
+    cout << "Velocidad del vehiculo     -> " << vel<< endl;
+    cout << "Tiempo estimado de entrega -> " << tf << endl;
+
+    return nI;
+}
+
+int main(){
+    cout << "Ejecucion int :" << endl;
+    int n1 = realizar_informe<int>();
+
+    cout << "\nEjecucion double :" << endl;
+    double n2 = realizar_informe<double>();
+
+    cout << "\nEjecucion long :" << endl;
+    long n3 = realizar_informe<long>();
+
+    return 0;
+}
--- Trabajo_10/CPP/Actividades/Actividad_2.cpp
+++ Trabajo_10/CPP/Actividades/Actividad_2.cpp
@@ -1,3 +1,51 @@
-//
-// Created by USER on 14/06/2024.
-//
+#include <iostream>
+using namespace std;
+#include <vector>
+
+
+
+class Ruta{
+protected:
+    string id_ruta;
+    string conductor;
+
+
+public:
+    Ruta(string id, string con){
+        this -> id_ruta = id;
+        this -> conductor = con;
+    }
+
+    template<typename T>
+    void vehiculo_asignado(T vehicle){
+        cout << "El vehiculo del conductor " << this -> conductor << " sera el: " << vehicle << endl;
+    }
+
+    template<typename G>
+    void designar_horario(){
+        G dis_total;
+        cout <<"Ingrese la distancia total: " << endl;
+        cin >> dis_total;
+        if(dis_total<100){
+            cout << "La entrega se realizará el jueves." << endl;
+        } else{
+            if(dis_total>101.5){
+                cout << "La entrega se realizará el viernes." << endl;
+            }
+        }
+    }
+
+};
+
+int main(){
+    Ruta ruta1("001", "Grossman");
+    Ruta ruta2("002", "Joaquin");
+
+    ruta1.vehiculo_asignado(5487);
+    ruta2.vehiculo_asignado("Carcocha");
+
+
+    ruta1.designar_horario<double>();
+    ruta2.designar_horario<int>();
+    return 0;
+}

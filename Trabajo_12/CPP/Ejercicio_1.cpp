e121b660668d79b52a29cdcf21379c64794b8230
Grossman Vargas <grossman.vargas@ucsm.edu.pe>
2024-06-25 09:27:58 -0500

    Ejercicio 1 agregado

--- Trabajo_12/CPP/Ejercicio_1.cpp
+++ Trabajo_12/CPP/Ejercicio_1.cpp
@@ -1,3 +1,54 @@
-//
-// Created by Grossman on 25/06/2024.
-//
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+
+string to_upper(const string &str) {
+    string result = str;
+    transform(result.begin(), result.end(), result.begin(), ::toupper);
+    return result;
+}
+
+int main() {
+    list<string> palabras;
+    string palabra;
+    char continuar;
+
+        cout << "Ingrese palabras (presione 'n' para terminar):" << endl;
+    while (true) {
+        cout << "Palabra: ";
+        cin >> palabra;
+        if (palabra == "n") break;
+        palabras.push_back(palabra);
+    }
+
+    palabras.sort();
+
+
+    cout << "Lista ordenada:" << endl;
+    for (const auto &p : palabras) {
+        cout << p << " ";
+    }
+    cout << endl;
+
+    string palabra_a_contar;
+    cout << "Ingrese la palabra a contar: ";
+    cin >> palabra_a_contar;
+    int count_palabra = count(palabras.begin(), palabras.end(), palabra_a_contar);
+    cout << "La palabra '" << palabra_a_contar << "' aparece " << count_palabra << " veces en la lista." << endl;
+
+
+    transform(palabras.begin(), palabras.end(), palabras.begin(), to_upper);
+
+
+    cout << "Lista resultante en mayúsculas:" << endl;
+    for (const auto &p : palabras) {
+        cout << p << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
